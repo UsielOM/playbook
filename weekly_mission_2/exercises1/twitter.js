93e4242f7fab7082eb342640a923c9082aa4ed77
UsielOM <usiel31197@outlook.es>
2022-04-18 20:32:17 -0500

    Semana3

---
+++ weekly_mission_2/exercises1/twitter.js
@@ -0,0 +1,15 @@
+const twitter = {
+    user: {
+        name: "Usiel",
+        username: "@Usix21_TW",
+        Biography: "Tripulante de la Mision Xpace",
+        location: "Ecatepec",
+        website: "usiel.com",
+        Birthday: "31/07/1999",
+        follows: "31",
+        folowers: "0"
+    },
+    trending_topic: {
+
+    }
+}
