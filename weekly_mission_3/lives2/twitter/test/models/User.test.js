562abfd2dbc15aa76960182e435df691f1f37fd1
UsielOM <usiel31197@outlook.es>
2022-04-20 20:52:50 -0500

    Requerimiento 2 fecha en atributos de user

--- weekly_mission_3/lives2/twitter/test/models/User.test.js
+++ weekly_mission_3/lives2/twitter/test/models/User.test.js
@@ -1,11 +1,19 @@
 const User = require('./../../app/models/User')
 
 describe("Prueba de jest", () => {
-    test("Prueba 1", () => {
+    test("Requerimiento 1: Creacion de User", () => {
 
-        const user = new User(1, "carlogimar", "usiel", "Bio", "datos", "actualziacion")
+        const user = new User(1, "usix", "usiel", "Bio")
         expect(user.id).toBe(1)
+        expect(user.username).toBe("usix")
 
 
+    })
+
+    test("Requerimiento 2: Fechas en atributos de User", () => {
+
+        const user = new User(2, "usix21", "usielOM", "Bio")
+        expect(user.dateCreated).not.toBeUndefined()
+        expect(user.lastUpdated).not.toBeUndefined()
     })
 })
