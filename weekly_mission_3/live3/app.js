638b28416bf0d75e58fa0664290db58a091cdc9a
UsielOM <usiel31197@outlook.es>
2022-04-21 20:35:13 -0500

    Agregando nueva ruta

--- weekly_mission_3/live3/app.js
+++ weekly_mission_3/live3/app.js
@@ -10,6 +10,17 @@ app.get('/', (req, res) => {
     res.send("Hola a todos mis explores!!! Lo quiero modificar")
 })
 
+
+
+
+//localhost:3000/launchx
+app.get('/launchx', (req, res) => {
+
+    res.send("Bienvenidos a lunchX")
+})
+
+
+
 app.listen(port, () => {
 
     console.log("Server Listo!")
