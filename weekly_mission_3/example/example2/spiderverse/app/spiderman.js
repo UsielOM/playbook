8269412024ed66a6f477a60afe673b271feb795a
UsielOM <usiel31197@outlook.es>
2022-04-23 13:34:08 -0500

    Creacion de la clase Spiderman

---
+++ weekly_mission_3/example/example2/spiderverse/app/spiderman.js
@@ -0,0 +1,11 @@
+class Spiderman {
+    constructor(nombre, edad, actor, nPeliculas, estudioCine) {
+        this.nombre = nombre
+        this.edad = edad
+        this.actor = actor
+        this.nPeliculas = nPeliculas
+        this.estudioCine = estudioCine
+    }
+}
+
+module.exports = Spiderman
